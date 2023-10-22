3cf21f0cead2f69fef1461d92f35edc0e61ddbdf
pusteugene <pustoviteugene@gmail.com>
2023-10-22 13:11:43 +0300

    add new class: Library

---
+++ src/main/java/Library.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        for (Book book : books) {
+            System.out.println(book);
+        }
+    }
+
+    public Book searchBookByTitle(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public void removeBookByIsbn(String isbn) {
+        books.removeIf(book -> book.getIsbn().equals(isbn));
+    }
+}
